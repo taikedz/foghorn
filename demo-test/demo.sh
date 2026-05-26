65c16946c3af0b53ceade3bb3f90591d849c7052
Tai Kedzierski <dch.tai@gmail.com>
2026-05-26 17:15:49 +0100

    Better demo

--- demo-test/demo.sh
+++ demo-test/demo.sh
@@ -13,21 +13,23 @@ main() {
         name="${2:-fog_default}"
         ip="${3:-192.168.42.0/24}"
         interval=300
-        /hostdata/foghorn --log ./${name}.log --database ./demo.${name}.db run ${ip} --interval ${interval}
+        /hostdata/foghorn --log ./${name}.log --database ./demo.${name}.db run ${ip} --interval ${interval} --altname "extraname-${name}"
         ;;
     start)
         cd "$HEREDIR"
         docker compose up -d
         sleep 2
         docker ps | grep demo-test || docker logs demo-test-fog1-1
-        ( set -x
-            : Example of querying a database file
-            "$HEREDIR/../foghorn" --database "$HEREDIR/demo.fog1.db" query --dump
-        )
+        
+        echo -e "\033[33;1m============ Example of querying a database file for all entries\033[0m"
+        (set -x; "$HEREDIR/../foghorn" --database "$HEREDIR/demo.fog1.db" query --dump)
+        echo -e "\033[33;1m============ Example of querying a database file for hosts compatible notation\033[0m"
+        (set -x; "$HEREDIR/../foghorn" --database "$HEREDIR/demo.fog1.db" query --hosts)
         ;;
     stop)
         cd "$HEREDIR"
         docker compose down
+        git clean -dfx "$HEREDIR"
         ;;
     build)
         cd "$HEREDIR"
