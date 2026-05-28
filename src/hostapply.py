dcb17c314af9257e35c5e49543ed72d0d10a4354
Tai Kedzierski <dch.tai@gmail.com>
2026-05-28 09:22:02 +0100

    Better etc-hosts management

--- src/hostapply.py
+++ src/hostapply.py
@@ -8,7 +8,12 @@ def apply_hosts(new_data_lines:list[str]):
     new_data = NL.join([f"{s}     # {TAG}" for s in new_data_lines])
 
     with open("/etc/hosts") as fh:
-        etc_data = ''.join([s for s in fh.readlines() if not TAG in s])
+        # Kepp only non-foghorn posts, we'll replace them below
+        etc_lines = [s for s in fh.readlines() if not TAG in s]
+        # Remove empty trailing lines
+        while etc_lines and etc_lines[-1].strip() == "":
+            etc_lines = etc_lines[:-1]
+        etc_data = ''.join(etc_lines)
 
     if os.path.exists("/tmp/etc-hosts"):
         if os.system("chown root:root /tmp/etc-hosts") != 0:
@@ -16,9 +21,11 @@ def apply_hosts(new_data_lines:list[str]):
 
     with open("/tmp/etc-hosts", "w") as fh:
         fh.write(etc_data)
+        fh.write(NL)
         fh.write(f"# =========== {TAG} ========== {NL}")
         fh.write(f"# Do not remove these tags --> {TAG} {NL}")
         fh.write(f"# ----------- {TAG} ---------- {NL}")
         fh.write(new_data)
+        fh.write(NL)
 
     shutil.move("/tmp/etc-hosts", "/etc/hosts")
